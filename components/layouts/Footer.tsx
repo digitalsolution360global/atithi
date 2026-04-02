5a0ffca17e0d55dccf5d5efc075b3443481addb2
Arun Verma <arunverma759954@gmail.com>
2026-04-02 10:16:38 +0530

    Remove Warranty link from footer and sync all changes

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -34,7 +34,6 @@ export default function Footer() {
             <li><Link href="/" className="hover:text-[#FACC15]">Home</Link></li>
             <li><Link href="/about" className="hover:text-[#FACC15]">About Us</Link></li>
             <li><Link href="/services" className="hover:text-[#FACC15]">Services</Link></li>
-            <li><Link href="/warranty" className="hover:text-[#FACC15]">Warranty</Link></li>
             <li><Link href="/gallery" className="hover:text-[#FACC15]">Gallery</Link></li>
             <li><Link href="/contact" className="hover:text-[#FACC15]">Contact</Link></li>
           </ul>
