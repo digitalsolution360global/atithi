6887131cdea1ee93dc5d13b140a0f9103d1d17b2
Arpit994 <arpitkumar994@gmail.com>
2026-05-19 16:25:04 +0530

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,6 +10,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 1,
     },
+
     {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
@@ -35,14 +36,63 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${baseUrl}/gallery`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
+      priority: 0.8,
     },
 
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
+      priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/blog/best-hotel-near-pratiksha-hospital-guwahati`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/blog/homestay-in-guwahati-vs-hotel-whats-better-near-pratiksha-hospital`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.64,
+    },
+
+    {
+      url: `${baseUrl}/blog/family-stay-in-guwahati-safe-comfortable-hotels-near-vip-road`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.64,
+    },
+
+    {
+      url: `${baseUrl}/blog/24-hours-hotel-guwahati`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.64,
+    },
+
+    {
+      url: `${baseUrl}/blog/top-reasons-budget-hotel-guwahati-near-vip-road`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.64,
+    },
+
+    {
+      url: `${baseUrl}/blog/best-homestay-guwahati-for-long-stays`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.64,
     },
   ]
 }
